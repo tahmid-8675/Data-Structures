64a39e0f6c7ac0be7ab7abf9fe6ed691e783eb20
tahmid-8675 <tahmidalam8765@gmail.com>
2024-07-02 18:36:02 +0600

    ps1

---
+++ Partial sum/partialsum-01.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+const int mx=2e5+123;
+int a[mx];//array initalized with 0
+
+ll presum[mx];
+
+
+main(){
+      optimize();
+    int n,q;
+
+    cin>>n>>q;
+
+    for(int i=1;i<=n;i++){
+
+        cin>>a[i];
+    }
+
+    for(int i=1;i<=n;i++){
+
+           // if(i-1==-1) for 0 based index
+           // continue;
+
+        presum[i]=presum[i-1]+a[i];
+    }
+
+  while(q--){
+
+        int l,r;
+
+        cin>>l>>r;
+
+        //l++,r++ for 0 based index
+
+        cout<<presum[r]-presum[l-1]<<endl;
+
+
+
+
+
+
+  }
+
+
+
+}
+
+//https://cses.fi/problemset/task/1646/
+
+
+    /*Input:
+
+8 4
+3 2 4 5 1 1 5 3
+2 4
+5 6
+1 8
+3 3   */
